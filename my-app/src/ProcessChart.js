61eec09454ea42c60a9aaf860fa2acbc80eb9872
Darshil Thakkar <121124627+DarshilThakkar@users.noreply.github.com>
2024-04-18 08:18:15 +0530

    UI change

--- my-app/src/ProcessChart.js
+++ my-app/src/ProcessChart.js
@@ -6,13 +6,12 @@ const ProcessChart = ({ processes }) => {
   const maxTime = Math.max(...processes.map(process => process.finishTime));
   const maxProcessIndex = processes.length;
 
-  const xScale = (time) => (time / maxTime) * 800; // Assuming 800px width for the chart
-  const yScale = (index) => (index / maxProcessIndex) * 400; // Assuming 400px height for the chart
+  const xScale = (time) => (time / maxTime) * 800;
+  const yScale = (index) => (index / maxProcessIndex) * 400;
 
   return (
     <div className="process-chart">
       <svg width="1000" height="500">
-        {/* X Axis */}
         <line x1="100" y1="450" x2="900" y2="450" />
         {processes.map((process, index) => (
           <text
@@ -26,7 +25,6 @@ const ProcessChart = ({ processes }) => {
         ))}
         <text x="500" y="495" textAnchor="middle">Time</text>
         
-        {/* Y Axis */}
         <line x1="100" y1="50" x2="100" y2="450" />
         <text x="20" y="250" textAnchor="middle">Process</text>
         {processes.map((process, index) => (
@@ -41,7 +39,6 @@ const ProcessChart = ({ processes }) => {
           </text>
         ))}
 
-        {/* Processes */}
         {processes.map((process, index) => (
           <rect
             key={process.name}
